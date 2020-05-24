ce141a63ceb31cd8a04421043ed573e4f750a364
leandrofranca <leandrofranca@gmail.com>
2020-05-24 01:20:55 -0300

    Melhoria no código e redução de passos com reutilização

--- main.py
+++ main.py
@@ -12,12 +12,11 @@ warnings.filterwarnings('ignore', r".*support Decimal objects natively",
                         SAWarning, r"^sqlalchemy\.sql\.sqltypes$")
 
 TZ = 'America/Fortaleza'
-NAP_TITLE = 'Cochilo'
-SLEEP_TITLE = 'Sono da noite'
-DIAPER_CHANGE_TYPE = 'diaper_change'
-SLEEPING_TYPE = 'sleeping'
-MAX_SLEEP_INTERVAL = 30
+NAP_UID = 'qpbgpcWOXOIRX0173470082348182010'
+SLEEP_UID = 'nydndmixbpEU27753063060301756015'
+BABY_BDAY = pd.Timestamp(2019, 12, 20).tz_localize('UTC').tz_convert(TZ)
 MAX_NAP_INTERVAL = 20
+MAX_SLEEP_INTERVAL = 60
 
 engine = create_engine('sqlite:///BabyDaybook.db')
 Session = sessionmaker()
@@ -30,14 +29,11 @@ Baby = Base.classes.babies
 Action = Base.classes.daily_actions
 Groups = Base.classes.groups
 
-NAP_UID = session.query(Groups).filter_by(
-    daily_action_type=SLEEPING_TYPE).filter_by(title=NAP_TITLE).first().uid
-SLEEP_UID = session.query(Groups).filter_by(
-    daily_action_type=SLEEPING_TYPE).filter_by(title=SLEEP_TITLE).first().uid
-BABY_BDAY = pd.to_datetime(int(float(session.query(
-    Baby).first().birthday)), unit='ms').tz_localize('UTC').tz_convert(TZ)
 
-ALL_DAILY_ACTIONS = session.query(Action).order_by(Action.start_millis)
+diapers_change = session.query(Action).order_by(Action.start_millis).filter_by(
+    type='diaper_change').all()
+sleeps = session.query(Action).order_by(Action.start_millis).filter_by(
+    type='sleeping').filter(Action.start_millis < Action.end_millis).all()
 
 
 def _convert_date_to_millis(date=None):
@@ -71,129 +67,116 @@ def _get_end_of_the_day(date):
     return date.normalize() + pd.Timedelta('1 day') - pd.Timedelta('1 ms')
 
 
+def _get_yesterday(date):
+    return date.normalize() - pd.Timedelta('1 day')
+
+
 def _get_now_in_millis():
     return int(pd.Timestamp.today().timestamp()*1000)
 
 
-diapers_change = ALL_DAILY_ACTIONS.filter_by(type=DIAPER_CHANGE_TYPE).all()
-sleeps = ALL_DAILY_ACTIONS.filter_by(type=SLEEPING_TYPE).filter(
-    Action.start_millis < Action.end_millis).all()
+join = dict()
+wrong_groups = dict()
 
-all_sleeps = []
-all_naps = []
-wrong_sleeps = []
-wrong_naps = []
-exclude = set()
-
-'''Guardando listas de dormidas e cochilos'''
 for date in pd.date_range(start=BABY_BDAY, end=pd.Timestamp.today(tz=TZ), tz=TZ, normalize=True, closed='left'):
-    start_of_the_day = _convert_date_to_millis(_get_start_of_the_day(date))
-    end_of_the_day = _convert_date_to_millis(_get_end_of_the_day(date))
-
-    diapers_change_of_the_day = list(filter(lambda diaper_change: diaper_change.start_millis >= start_of_the_day and
-                                            diaper_change.start_millis <= end_of_the_day,
-                                            diapers_change))
-
     try:
-        first_diaper_change = diapers_change_of_the_day[0]
-        last_diaper_change = diapers_change_of_the_day[-1]
+        start_of_today = _convert_date_to_millis(_get_start_of_the_day(date))
+        end_of_today = _convert_date_to_millis(_get_end_of_the_day(date))
+        start_of_yesterday = _convert_date_to_millis(
+            _get_start_of_the_day(_get_yesterday(date)))
+        end_of_yesterday = _convert_date_to_millis(
+            _get_end_of_the_day(_get_yesterday(date)))
 
-        sleeps_of_the_day = list(filter(lambda sleep: (sleep.start_millis >= _convert_date_to_millis(date) and
-                                                       sleep.start_millis <= end_of_the_day) and
-                                        (sleep.start_millis < first_diaper_change.start_millis or
-                                         sleep.start_millis > last_diaper_change.start_millis), sleeps))
-        wrong_sleeps += list(filter(lambda sleep: sleep.group_uid != SLEEP_UID,
-                                    sleeps_of_the_day))
-
-        naps_of_the_day = list(filter(lambda sleep: (sleep.start_millis > first_diaper_change.start_millis and
-                                                     sleep.start_millis < last_diaper_change.start_millis),
-                                      sleeps))
-        wrong_naps += list(filter(lambda sleep: (sleep.group_uid != NAP_UID),
-                                  naps_of_the_day))
-
-        all_sleeps += sleeps_of_the_day
-        all_naps += naps_of_the_day
-    except:
+        diapers_change_of_today = list(filter(
+            lambda diaper_change:
+            diaper_change.start_millis >= start_of_today and
+            diaper_change.start_millis <= end_of_today,
+            diapers_change
+        ))
+        first_diaper_change_of_today = diapers_change_of_today[0]
+        diapers_change_of_yesterday = list(filter(
+            lambda diaper_change:
+            diaper_change.start_millis >= start_of_yesterday and
+            diaper_change.start_millis <= end_of_yesterday,
+            diapers_change
+        ))
+        first_diaper_change_of_yesterday = diapers_change_of_yesterday[0]
+        last_diaper_change_of_yesterday = diapers_change_of_yesterday[-1]
+        day_period = {
+            'start': first_diaper_change_of_yesterday.start_millis,
+            'end': last_diaper_change_of_yesterday.start_millis
+        }
+        night_period = {
+            'start': last_diaper_change_of_yesterday.start_millis,
+            'end': first_diaper_change_of_today.start_millis
+        }
+        daily_naps_of_yesterday = list(filter(
+            lambda nap:
+            nap.start_millis > day_period['start'] and
+            nap.end_millis < day_period['end'],
+            sleeps
+        ))
+        night_sleeps_of_last_night = list(filter(
+            lambda sleep:
+            sleep.start_millis > night_period['start'] and
+            sleep.end_millis < night_period['end'],
+            sleeps
+        ))
+        for daily_nap_of_yesterday in daily_naps_of_yesterday:
+            try:
+                join[daily_nap_of_yesterday] = list(filter(
+                    lambda nap:
+                    nap.start_millis > daily_nap_of_yesterday.end_millis and
+                    _convert_millis_to_minutes(
+                        nap.start_millis - daily_nap_of_yesterday.end_millis) <= MAX_NAP_INTERVAL,
+                    daily_naps_of_yesterday
+                ))[0]
+                if join[daily_nap_of_yesterday].group_uid != "":
+                    wrong_groups[join[daily_nap_of_yesterday]] = ""
+                if daily_nap_of_yesterday.group_uid != NAP_UID:
+                    wrong_groups[daily_nap_of_yesterday] = NAP_UID
+            except IndexError:
+                continue
+        for night_sleep_of_last_night in night_sleeps_of_last_night:
+            try:
+                join[night_sleep_of_last_night] = list(filter(
+                    lambda sleep:
+                    sleep.start_millis > night_sleep_of_last_night.end_millis and
+                    _convert_millis_to_minutes(
+                        sleep.start_millis - night_sleep_of_last_night.end_millis) <= MAX_SLEEP_INTERVAL,
+                    night_sleeps_of_last_night
+                ))[0]
+                if join[night_sleep_of_last_night].group_uid != "":
+                    wrong_groups[join[night_sleep_of_last_night]] = ""
+                if night_sleep_of_last_night.group_uid != SLEEP_UID:
+                    wrong_groups[night_sleep_of_last_night] = SLEEP_UID
+            except IndexError:
+                continue
+    except IndexError:
         continue
 
-all_sleeps_and_naps = list(set().union(all_sleeps, all_naps))
-
-print('*'*100)
-print('Removendo grupo das sobreposições de horário')
-for sleep in all_sleeps_and_naps:
-    nexts = list(filter(lambda next: (next.uid != sleep.uid) and
-                        (next.group_uid == sleep.group_uid) and
-                        (next.start_millis >= sleep.start_millis) and
-                        (next.end_millis <= sleep.end_millis),
-                        all_sleeps_and_naps))
-    for next in nexts:
-        next.group_uid = ""
-        next.updated_millis = _get_now_in_millis()
-        exclude.add(next)
-        print(_calc_interval(sleep), _calc_interval(next))
-
-print('*'*100)
-print('Corrigindo grupo das dormidas')
-for sleep in wrong_sleeps:
-    if sleep in exclude:
-        continue
-
-    sleep.group_uid = SLEEP_UID
+print('-'*100)
+print('Correção de grupos')
+print('-'*100)
+for sleep, group_uid in wrong_groups.items():
+    sleep.group_uid = group_uid
     sleep.updated_millis = _get_now_in_millis()
-    print(_calc_interval(sleep), SLEEP_TITLE)
+    print(_calc_interval(sleep), 'mudará o grupo para', "\"" + group_uid + "\"")
 
-print('*'*100)
-print('Corrigindo grupo dos cochilos')
-for nap in wrong_naps:
-    if nap in exclude:
-        continue
-
-    nap.group_uid = NAP_UID
-    nap.updated_millis = _get_now_in_millis()
-    print(_calc_interval(nap), NAP_TITLE)
-
-correct_pause = dict()
-
-print('*'*100)
-print("Verificando quais dormidas ou cochilos podem ser mesclados")
-for sleep in all_sleeps_and_naps:
-    if sleep in exclude:
-        continue
-
-    try:
-        next = list(filter(lambda next: (next.group_uid == sleep.group_uid) and
-                           (next.start_millis > sleep.end_millis) and
-                           (((_convert_millis_to_minutes(next.start_millis - sleep.end_millis) <= MAX_SLEEP_INTERVAL) and
-                             (sleep.group_uid == SLEEP_UID)) or
-                            ((_convert_millis_to_minutes(next.start_millis - sleep.end_millis) <= MAX_NAP_INTERVAL) and
-                             (sleep.group_uid == NAP_UID))),
-                           all_sleeps_and_naps))[0]
-
-        pause_millis = _convert_millis_to_minutes(
-            next.start_millis - sleep.end_millis) * 60 * 1000
-
-        exclude.add(next)
-        correct_pause[sleep] = [next.end_millis, pause_millis]
-    except:
-        pass
-
-print('*'*100)
-print("Mesclando dormidas intermediárias com interrupções")
-for sleep, value in correct_pause.items():
-    print("Antes: ", _calc_interval(sleep))
-    sleep.end_millis = value[0]
-    sleep.pause_millis += value[1]
-    sleep.updated_millis = _get_now_in_millis()
-    print("Depois: ", _calc_interval(sleep), "Intervalo (min)",
+print('-'*100)
+print('Correção de gaps')
+print('-'*100)
+for sleep, gap in join.items():
+    print(" Antes:", _calc_interval(sleep), "Intervalo (min)",
           _convert_millis_to_minutes(sleep.pause_millis))
-    print()
-
-print('*'*100)
-print("Removendo grupo das dormidas intermediárias")
-for sleep in exclude:
-    print(_calc_interval(sleep))
-    sleep.group_uid = ""
+    pause_millis = _convert_millis_to_minutes(
+        gap.start_millis - sleep.end_millis) * 60 * 1000
+    sleep.end_millis = gap.end_millis
+    sleep.pause_millis += pause_millis + gap.pause_millis
     sleep.updated_millis = _get_now_in_millis()
+    print("Depois:", _calc_interval(sleep), "Intervalo (min)",
+          _convert_millis_to_minutes(sleep.pause_millis))
+    print('-'*3)
 
 # print(session.dirty)
 # session.commit()
