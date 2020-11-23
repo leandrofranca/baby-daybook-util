b1e11eae6a839eb1ce93060e3891bc1ec4a4ee9e
leandrofranca <leandrofranca@gmail.com>
2020-11-23 00:39:08 -0300

    Diversas melhorias e usando informações da Agenda Edu

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import re
 import sys
+import uuid 
 import time
 import warnings
 
+import mechanicalsoup
 import pandas as pd
 from sqlalchemy import and_, create_engine
 from sqlalchemy.exc import SAWarning
@@ -12,11 +15,9 @@ warnings.filterwarnings('ignore', r".*support Decimal objects natively",
                         SAWarning, r"^sqlalchemy\.sql\.sqltypes$")
 
 TZ = 'America/Fortaleza'
-NAP_UID = 'qpbgpcWOXOIRX0173470082348182010'
-SLEEP_UID = 'nydndmixbpEU27753063060301756015'
 BABY_BDAY = pd.Timestamp(2019, 12, 20).tz_localize('UTC').tz_convert(TZ)
 MAX_NAP_INTERVAL = 20
-MAX_SLEEP_INTERVAL = 60
+MAX_SLEEP_INTERVAL = 30
 
 engine = create_engine('sqlite:///BabyDaybook.db')
 Session = sessionmaker()
@@ -30,10 +31,28 @@ Action = Base.classes.daily_actions
 Groups = Base.classes.groups
 
 
-diapers_change = session.query(Action).order_by(Action.start_millis).filter_by(
-    type='diaper_change').all()
-sleeps = session.query(Action).order_by(Action.start_millis).filter_by(
-    type='sleeping').filter(Action.start_millis < Action.end_millis).all()
+diapers = session.query(Action).filter_by(type='diaper_change').order_by(Action.start_millis).all()
+groups = session.query(Groups).all()
+sleeps = session.query(Action).filter_by(type='sleeping').filter(Action.start_millis < Action.end_millis).order_by(Action.start_millis).all()
+
+
+def get_group_uid_from_title(title=None):
+    return list(filter(
+        lambda group:
+        group.title == title,
+        groups
+    ))[0].uid
+
+
+def get_group_title_from_uid(uid=None):
+    return list(filter(
+        lambda group:
+        group.uid == uid,
+        groups
+    ))[0].title
+
+
+foods = session.query(Action).filter_by(type='food').filter(Action.group_uid == get_group_uid_from_title('Fruta')).order_by(Action.start_millis).all()
 
 
 def _convert_date_to_millis(date=None):
@@ -83,23 +102,21 @@ for date in pd.date_range(start=BABY_BDAY, end=pd.Timestamp.today(tz=TZ), tz=TZ,
     try:
         start_of_today = _convert_date_to_millis(_get_start_of_the_day(date))
         end_of_today = _convert_date_to_millis(_get_end_of_the_day(date))
-        start_of_yesterday = _convert_date_to_millis(
-            _get_start_of_the_day(_get_yesterday(date)))
-        end_of_yesterday = _convert_date_to_millis(
-            _get_end_of_the_day(_get_yesterday(date)))
+        start_of_yesterday = _convert_date_to_millis(_get_start_of_the_day(_get_yesterday(date)))
+        end_of_yesterday = _convert_date_to_millis(_get_end_of_the_day(_get_yesterday(date)))
 
         diapers_change_of_today = list(filter(
             lambda diaper_change:
             diaper_change.start_millis >= start_of_today and
             diaper_change.start_millis <= end_of_today,
-            diapers_change
+            diapers
         ))
         first_diaper_change_of_today = diapers_change_of_today[0]
         diapers_change_of_yesterday = list(filter(
             lambda diaper_change:
             diaper_change.start_millis >= start_of_yesterday and
             diaper_change.start_millis <= end_of_yesterday,
-            diapers_change
+            diapers
         ))
         first_diaper_change_of_yesterday = diapers_change_of_yesterday[0]
         last_diaper_change_of_yesterday = diapers_change_of_yesterday[-1]
@@ -125,8 +142,8 @@ for date in pd.date_range(start=BABY_BDAY, end=pd.Timestamp.today(tz=TZ), tz=TZ,
         ))
         for daily_nap_of_yesterday in daily_naps_of_yesterday:
             try:
-                if daily_nap_of_yesterday.group_uid != NAP_UID:
-                    groups[daily_nap_of_yesterday] = NAP_UID
+                if daily_nap_of_yesterday.group_uid != get_group_uid_from_title('Soneca'):
+                    groups[daily_nap_of_yesterday] = get_group_uid_from_title('Soneca')
                 intersections[daily_nap_of_yesterday] = list(filter(
                     lambda nap:
                     nap.start_millis > daily_nap_of_yesterday.start_millis and
@@ -134,8 +151,8 @@ for date in pd.date_range(start=BABY_BDAY, end=pd.Timestamp.today(tz=TZ), tz=TZ,
                     (nap.pause_millis > 0 or nap.group_uid != ''),
                     daily_naps_of_yesterday
                 ))[0]
-                if intersections[daily_nap_of_yesterday].group_uid != '':
-                    groups[intersections[daily_nap_of_yesterday]] = ''
+                # if intersections[daily_nap_of_yesterday].group_uid != '':
+                #     groups[intersections[daily_nap_of_yesterday]] = ''
             except IndexError:
                 pass
 
@@ -143,19 +160,18 @@ for date in pd.date_range(start=BABY_BDAY, end=pd.Timestamp.today(tz=TZ), tz=TZ,
                 junctions[daily_nap_of_yesterday] = list(filter(
                     lambda nap:
                     nap.start_millis > daily_nap_of_yesterday.end_millis and
-                    _convert_millis_to_minutes(
-                        nap.start_millis - daily_nap_of_yesterday.end_millis) <= MAX_NAP_INTERVAL,
+                    _convert_millis_to_minutes(nap.start_millis - daily_nap_of_yesterday.end_millis) <= MAX_NAP_INTERVAL,
                     daily_naps_of_yesterday
                 ))[0]
-                if junctions[daily_nap_of_yesterday].group_uid != '':
-                    groups[junctions[daily_nap_of_yesterday]] = ''
+                # if junctions[daily_nap_of_yesterday].group_uid != '':
+                #     groups[junctions[daily_nap_of_yesterday]] = ''
             except IndexError:
                 pass
 
         for night_sleep_of_last_night in night_sleeps_of_last_night:
             try:
-                if night_sleep_of_last_night.group_uid != SLEEP_UID:
-                    groups[night_sleep_of_last_night] = SLEEP_UID
+                if night_sleep_of_last_night.group_uid != get_group_uid_from_title('Sono da noite'):
+                    groups[night_sleep_of_last_night] = get_group_uid_from_title('Sono da noite')
                 intersections[night_sleep_of_last_night] = list(filter(
                     lambda sleep:
                     sleep.start_millis > night_sleep_of_last_night.start_millis and
@@ -163,8 +179,8 @@ for date in pd.date_range(start=BABY_BDAY, end=pd.Timestamp.today(tz=TZ), tz=TZ,
                     (sleep.pause_millis > 0 or sleep.group_uid != ''),
                     night_sleeps_of_last_night
                 ))[0]
-                if intersections[night_sleep_of_last_night].group_uid != '':
-                    groups[intersections[night_sleep_of_last_night]] = ''
+                # if intersections[night_sleep_of_last_night].group_uid != '':
+                #     groups[intersections[night_sleep_of_last_night]] = ''
             except IndexError:
                 pass
 
@@ -172,12 +188,11 @@ for date in pd.date_range(start=BABY_BDAY, end=pd.Timestamp.today(tz=TZ), tz=TZ,
                 junctions[night_sleep_of_last_night] = list(filter(
                     lambda sleep:
                     sleep.start_millis > night_sleep_of_last_night.end_millis and
-                    _convert_millis_to_minutes(
-                        sleep.start_millis - night_sleep_of_last_night.end_millis) <= MAX_SLEEP_INTERVAL,
+                    _convert_millis_to_minutes(sleep.start_millis - night_sleep_of_last_night.end_millis) <= MAX_SLEEP_INTERVAL,
                     night_sleeps_of_last_night
                 ))[0]
-                if junctions[night_sleep_of_last_night].group_uid != '':
-                    groups[junctions[night_sleep_of_last_night]] = ''
+                # if junctions[night_sleep_of_last_night].group_uid != '':
+                #     groups[junctions[night_sleep_of_last_night]] = ''
             except IndexError:
                 pass
     except IndexError:
@@ -187,30 +202,25 @@ print('-'*100)
 print('Transferindo interrupções para dormida principal')
 print('-'*100)
 for sleep, internal in intersections.items():
-    print(' Inicial:', _calc_interval(sleep), 'intervalo (min)',
-          _convert_millis_to_minutes(sleep.pause_millis), '+')
-    print(' Interna:', _calc_interval(internal), 'intervalo (min)',
-          _convert_millis_to_minutes(internal.pause_millis), '=')
+    print(' Inicial:', _calc_interval(sleep), 'intervalo (min)', _convert_millis_to_minutes(sleep.pause_millis), '+')
+    print(' Interna:', _calc_interval(internal), 'intervalo (min)', _convert_millis_to_minutes(internal.pause_millis), '=')
 
     sleep.pause_millis += internal.pause_millis
     sleep.updated_millis = _get_now_in_millis()
 
-    print('   Final:', _calc_interval(sleep), 'intervalo (min)',
-          _convert_millis_to_minutes(sleep.pause_millis))
+    print('   Final:', _calc_interval(sleep), 'intervalo (min)', _convert_millis_to_minutes(sleep.pause_millis))
     print('-'*3)
 
 print('-'*100)
 print('Removendo pausas das interseções')
 print('-'*100)
 for sleep in intersections.values():
-    print(' Antes:', _calc_interval(sleep), 'intervalo (min)',
-          _convert_millis_to_minutes(sleep.pause_millis))
+    print(' Antes:', _calc_interval(sleep), 'intervalo (min)', _convert_millis_to_minutes(sleep.pause_millis))
 
     sleep.pause_millis = 0
     sleep.updated_millis = _get_now_in_millis()
 
-    print('Depois:', _calc_interval(sleep), 'intervalo (min)',
-          _convert_millis_to_minutes(sleep.pause_millis))
+    print('Depois:', _calc_interval(sleep), 'intervalo (min)', _convert_millis_to_minutes(sleep.pause_millis))
     print('-'*3)
 
 print('-'*100)
@@ -218,8 +228,7 @@ print('Correção de grupos')
 print('-'*100)
 for sleep, group_uid in groups.items():
     if sleep.group_uid != group_uid:
-        print(sleep.uid, _calc_interval(sleep), 'grupo atual', '\"' + sleep.group_uid + '\"', 'mudará o grupo para',
-              '\"' + group_uid + '\"')
+        print(_calc_interval(sleep), get_group_title_from_uid(sleep.group_uid), get_group_title_from_uid(group_uid))
         sleep.group_uid = group_uid
         sleep.updated_millis = _get_now_in_millis()
 
@@ -227,18 +236,42 @@ print('-'*100)
 print('Correção de gaps')
 print('-'*100)
 for sleep, gap in junctions.items():
-    print(' Antes:', _calc_interval(sleep), 'intervalo (min)',
-          _convert_millis_to_minutes(sleep.pause_millis))
-    pause_millis = _convert_millis_to_minutes(
-        gap.start_millis - sleep.end_millis) * 60 * 1000
+    print(' Antes:', _calc_interval(sleep), 'intervalo (min)', _convert_millis_to_minutes(sleep.pause_millis))
+    pause_millis = _convert_millis_to_minutes(gap.start_millis - sleep.end_millis) * 60 * 1000
     sleep.end_millis = gap.end_millis
     sleep.pause_millis += pause_millis + gap.pause_millis
     sleep.updated_millis = _get_now_in_millis()
-    print('Depois:', _calc_interval(sleep), 'intervalo (min)',
-          _convert_millis_to_minutes(sleep.pause_millis))
+    print('Depois:', _calc_interval(sleep), 'intervalo (min)', _convert_millis_to_minutes(sleep.pause_millis))
     print('-'*3)
 
-# print(session.dirty)
+print('-'*100)
+print('Transformação de Fruta em Lanche')
+print('-'*100)
+for f in foods:
+    print(_convert_millis_to_date(f.start_millis), get_group_title_from_uid(f.group_uid), "Lanche")
+    f.group_uid = get_group_uid_from_title('Lanche')
+    f.updated_millis = _get_now_in_millis()
+
+print('-'*100)
+print('Trazendo informação da creche')
+print('-'*100)
+browser = mechanicalsoup.StatefulBrowser()
+browser.open("https://responsavel.agendaedu.com/")
+browser.select_form('form[action="/responsibles/sign_in"]')
+browser["responsible[login]"] = ""
+browser["responsible[password]"] = ""
+browser.submit_selected()
+p = re.compile('(\\d{2}:\\d{2})')
+for date in pd.date_range(start=pd.Timestamp('2020-09-14', tz='America/Fortaleza'), end=pd.Timestamp.today(tz='America/Fortaleza'), freq='B', tz='America/Fortaleza'):
+    browser.open("https://responsavel.agendaedu.com/responsibles/daily_summaries/%s" % date.strftime('%Y-%m-%d'))
+    page = browser.get_current_page().getText()
+    result = p.findall(page)
+    if len(result) >= 4:
+        print(date.strftime('%Y-%m-%d'), 'uid', uuid.uuid1().hex, 'user_uid', 'S2gBcXYovNfOAR4lflCUGEGH5vr2', 'baby_uid', 'uNWEPUD7531142628466708417801218',
+              'type', 'sleeping', 'start_millis', 'end_millis', 'group_uid', get_group_uid_from_title('Soneca'), 'updated_millis', _get_now_in_millis())
+        print(result)
+
+
 # session.commit()
 session.close()
 
